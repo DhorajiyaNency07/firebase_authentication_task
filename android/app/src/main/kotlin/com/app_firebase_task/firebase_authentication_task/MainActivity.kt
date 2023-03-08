a9d38ae2a489884f8f46536f3ecc966b04ceff98
DhorajiyaNency <dhorajiyanency@gmail.com>
2023-03-08 20:04:07 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/app_firebase_task/firebase_authentication_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app_firebase_task.firebase_authentication_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'google_sign_in.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+
+///
+///
+// Platform  Firebase App Id
+// web       1:208133681681:web:94b524daf2d0085ffaf7b0
+// android   1:208133681681:android:ccf9df65aa6df027faf7b0
+// ios       1:208133681681:ios:32517f818f3507a8faf7b0
+
