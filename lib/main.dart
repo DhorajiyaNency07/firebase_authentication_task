88e8e3833df8a5188b5f79682818492eff6c1013
DhorajiyaNency <dhorajiyanency@gmail.com>
2023-03-16 12:45:12 +0530

    16/03/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
-import 'google_sign_in.dart';
+import 'home_screen.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
